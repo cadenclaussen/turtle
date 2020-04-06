0fbcca38520c2be63713ce794a1e22651f57d7cf
Caden Claussen <caden.claussen@gmail.com>
2020-04-06 16:08:28 -0700

    lesson 1.3

---
+++ variables.py
@@ -0,0 +1,16 @@
+import time
+banana = 5
+caden = 7
+name = "Caden (its me!!!)"
+print(banana)
+banana = "w0w0w0w0w"
+print(banana)
+banana = 5
+print(name)
+print(3 * 200 - 3 * 2000000 * 1/2 + 5/6 / 4/5)
+print(banana * caden - 60606060 * 2)
+friend = "I" "l" "i" "y" "a" " and " "N" "i" "c" "o" " are some of my friends."
+print(friend)
+rocks = ["diamond", "iron", "gold", "saphire", "ruby"]
+rocks[1] = "Silver"
+print(rocks) 
