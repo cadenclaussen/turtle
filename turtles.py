bbd5b808c9d17a8170d681a0f1a07a975bb99a41
Caden Claussen <caden.claussen@gmail.com>
2020-04-06 15:18:19 -0700

    lesson 1.2

---
+++ turtles.py
@@ -0,0 +1,18 @@
+import turtle
+import time
+t = turtle.Pen()
+t.shape("turtle")
+t.color("purple")
+t.reset()
+t.forward(100)
+t.width(10)
+time.sleep(1)
+t.left(90)
+time.sleep(1)
+t.reset()
+t.color("green")
+t.circle(50)
+t.circle(-50)
+
+
+
